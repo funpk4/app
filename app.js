09a4e8efdd8ea91c6522c65e13dbee4ff0ded973
funpk4 <83115911+funpk4@users.noreply.github.com>
2021-05-15 17:47:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
 app.use(express.json());
-
-
 const port = process.env.PORT || 4000;
+app.get("/", (req, res)=>
+{
+  res.send("Hello World");
+})
 app.listen(port, () => {
   console.log("server up and running on PORT :", port);
 });
